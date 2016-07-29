d79bac6ae5872a7eb9e6145bf7e655faf48386ce
Shantanu Rahman <rahmanshantanu69@gmail.com>
2016-07-29 17:08:18 +0600

    The project is now WORKING. Now it is time to refactor code

--- arduino_utils.py
+++ arduino_utils.py
@@ -3,12 +3,18 @@
 """
 
 import json
-# import serial
+import pyfirmata
 
 from syntax.lexicon import scan
 from syntax.parser import parse_sentence, ParserError
 
-MAPPING = json.loads(open("mapping.json").read())["items"]
+mappingfile = open("mapping.json")
+config_data = json.loads(mappingfile.read())
+mappingfile.close()
+
+MAPPING = config_data["items"]
+PORT = input("port> ")
+BOARD = pyfirmata.Arduino(PORT);
 
 def parse_n_send_command(msg_body):
     """ Parse the command and send to arduino."""
@@ -30,5 +36,6 @@ def parse_n_send_command(msg_body):
             item = MAPPING[item]
 
         print(item, action)
+        BOARD.digital[item].write(action)
 
     return
